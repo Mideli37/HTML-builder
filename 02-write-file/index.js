88abda5e70669917357b3ac98a5be4d0956dfc3a
Mideli <muzipovanastia@gmail.com>
2024-01-16 22:41:36 +0200

    feat: add solution for 02 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const os = require('node:os');
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const dstPath = path.join(__dirname, 'answers.txt');
+
+const writeStream = fs.createWriteStream(dstPath);
+
+console.log('Hello! To exit use "exit" or ctrl+c');
+
+function ask() {
+  rl.question('What do you want to write down in the file? >> ', (answer) => {
+    if (answer == 'exit') {
+      rl.close();
+    } else {
+      writeStream.write(`${answer}${os.EOL}`);
+      ask();
+    }
+  });
+}
+
+rl.on('close', function () {
+  console.log(`${os.EOL}Your file is completed! Goodbye!`);
+});
+
+ask();
