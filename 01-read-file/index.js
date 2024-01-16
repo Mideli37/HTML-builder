eeb6c5ab0618683ddbe2607a262efd9a0861e4a0
Mideli <muzipovanastia@gmail.com>
2024-01-16 21:55:33 +0200

    feat: add solution for first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const path = require('node:path');
+const fs = require('node:fs');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const fileReadStream = fs.createReadStream(filePath);
+fileReadStream.on('data', (data) => {
+  console.log(data.toString());
+});
