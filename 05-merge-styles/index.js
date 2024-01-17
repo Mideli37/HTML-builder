127403b09279cada28a70452d96e56e0e6b119d6
Mideli <muzipovanastia@gmail.com>
2024-01-17 20:18:42 +0200

    feat: add solution for 05 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,38 @@
+const { readdir } = require('node:fs/promises');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const srcFolder = 'styles';
+const dstFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
+
+async function getFilesDirents(folder) {
+  const dirents = await readdir(path.join(__dirname, folder), {
+    withFileTypes: true,
+  });
+  return dirents;
+}
+
+async function buildCSSBundle(srcFolder, dstPath) {
+  let files = await getFilesDirents(srcFolder);
+  files = files.filter((file) => {
+    const filePath = path.join(file.path, file.name);
+    const fileExt = path.extname(filePath);
+    return fileExt === '.css';
+  });
+
+  const writeStream = fs.createWriteStream(dstPath);
+
+  for (let i = 0; i < files.length; i += 1) {
+    const file = files[i];
+    const filePath = path.join(file.path, file.name);
+    const fileExt = path.extname(filePath);
+    if (fileExt === '.css') {
+      const readStream = fs.createReadStream(filePath);
+      readStream.on('data', (data) => {
+        writeStream.write(data.toString());
+      });
+    }
+  }
+}
+
+buildCSSBundle(srcFolder, dstFilePath);
